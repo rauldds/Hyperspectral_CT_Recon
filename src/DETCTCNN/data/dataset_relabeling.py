2b808bf84f9b53b73c2770c22337baded32bae78
luisdavid64 <luisdavid007@gmail.com>
2023-06-26 12:56:33 +0200

    Added default path to relabeller

--- src/DETCTCNN/data/dataset_relabeling.py
+++ src/DETCTCNN/data/dataset_relabeling.py
@@ -8,7 +8,7 @@ from music_2d_labels import MUSIC_2D_LABELS,MUSIC_2D_SAMPLES
 
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument("-d", "--dataset", help="dataset path", type=str)
+argParser.add_argument("-d", "--dataset", help="dataset path", type=str, default="../../../MUSIC2D_HDF5")
 
 args = argParser.parse_args()
 print("DATASET PATH: %s" % args.dataset)
