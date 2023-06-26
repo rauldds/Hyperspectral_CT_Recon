4e6433bbbcecc0bfcee6c4ba24e194e418120a9e
luisdavid64 <luisdavid007@gmail.com>
2023-06-26 20:19:03 +0200

    Added number of labels to train loop

--- src/DETCTCNN/model/train.py
+++ src/DETCTCNN/model/train.py
@@ -2,6 +2,8 @@ from argparse import ArgumentParser
 from losses import WeightedLoss
 from model import get_model
 import torch
+
+import music_2d_labels
 from ..data.music_2d_dataset import MUSIC2DDataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -57,7 +59,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_root", type=str, default="../../../MUSIC2D_HDF5", help="Data root directory")
     parser.add_argument("--epochs", type=int, default=200, help="Number of maximum training epochs")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
-    parser.add_argument("--n_labels", type=int, default=11, help="Number of labels for final layer")
+    parser.add_argument("--n_labels", type=int, default=music_2d_labels.size, help="Number of labels for final layer")
     parser.add_argument("--lr", type=int, default=0.00005, help="Learning rate")
     args = parser.parse_args()
     main(args)
