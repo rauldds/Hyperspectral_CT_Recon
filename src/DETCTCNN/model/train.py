f073530c3448670cf22e2f465c7ab81722247638
luisdavid64 <luisdavid007@gmail.com>
2023-06-28 13:25:39 +0200

    Missing channel separation in segs

--- src/DETCTCNN/model/train.py
+++ src/DETCTCNN/model/train.py
@@ -33,7 +33,7 @@ def main(hparams):
     # ])
     transform = None
     dataset = MUSIC2DDataset(root=hparams.data_root,partition="train",spectrum="reducedSpectrum", transform=transform)
-    train_loader = DataLoader(dataset, batch_size=1)
+    train_loader = DataLoader(dataset, batch_size=hparams.batch_size)
     optimizer = torch.optim.Adam(model.parameters(), betas=([0.9, 0.999]), lr = hparams.lr)
 
     from losses import DiceLoss
@@ -73,7 +73,7 @@ def main(hparams):
 if __name__ == "__main__":
     parser = ArgumentParser()
 
-    parser.add_argument("--data_root", type=str, default="../../../MUSIC2D_HDF5", help="Data root directory")
+    parser.add_argument("--data_root", type=str, default="/Users/luisreyes/Courses/MLMI/Hyperspectral_CT_Recon/MUSIC2D_HDF5", help="Data root directory")
     parser.add_argument("--epochs", type=int, default=700, help="Number of maximum training epochs")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
     parser.add_argument("--n_labels", type=int, default=LABELS_SIZE, help="Number of labels for final layer")
