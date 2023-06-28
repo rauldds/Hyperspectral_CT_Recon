7d302f613db4300c4c288cd21da1461740a2bffc
luisdavid64 <luisdavid007@gmail.com>
2023-06-28 20:57:18 +0200

    Image should be working

--- src/DETCTCNN/model/utils.py
+++ src/DETCTCNN/model/utils.py
@@ -2,6 +2,7 @@ import PIL
 import os
 import numpy as np
 import seaborn as sns
+from PIL import Image
 
 def colorize_mask(mask, palette):
     zero_pad = 256 * 3 - len(palette)
@@ -13,8 +14,12 @@ def colorize_mask(mask, palette):
 
 def image_from_segmentation(prediction,no_classes):
 	# Saves the image, the model output and the results after the post processing
-    palette = sns.color_palette('hls', no_classes)
+    palette = np.rint(np.array(sns.color_palette('hls', no_classes)) * 255)
     mask = prediction.argmax(1).numpy().squeeze()
-    colored_image = np.take(palette, mask, axis=0).astype(np.uint8)
-    colored_image = colored_image.transpose(2,0,1)
+    colored_image = palette[mask]
+    colored_image = colored_image.astype(np.uint8)
+    to_save = colored_image.reshape(mask.shape[0], mask.shape[1], 3)
+    im = Image.fromarray(to_save)
+    im.save("example.jpeg")
+    colored_image = colored_image.reshape(3, mask.shape[0], mask.shape[1])
     return colored_image
